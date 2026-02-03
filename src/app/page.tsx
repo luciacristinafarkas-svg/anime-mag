1a4a5708745facd4f93e82ffaec3708157a77c34
luciacristinafarkas-svg <luciacristinafarkas@gmail.com>
2026-02-03 13:23:41 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{maxWidth:720, margin:"40px auto", padding:16}}>
+      <h1>Anime Magazine MVP</h1>
+      <a href="/create">Create</a>
+    </main>
+  );
+}
